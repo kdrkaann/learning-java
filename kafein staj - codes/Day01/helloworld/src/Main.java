4e6058220fdffb5f88c887ee18d63e8bd1e1e884
Kadir Kaan Atalay <102871607+kdrkaann@users.noreply.github.com>
2022-07-18 21:48:48 +0300

    Delete kafein staj - codes/Day01/helloworld directory

--- kafein staj - codes/Day01/helloworld/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("I'm Kadir Kaan.");
-        System.out.println("I've finished my " + 2 + "year at Kadir Has University");
-        System.out.println("I'm " + (12+8) + " years old.");
-    }
-}
