671a0d3c0810157d78a2dc7c432d878f28e77e44
Kadir Kaan Atalay <102871607+kdrkaann@users.noreply.github.com>
2022-09-19 23:53:37 +0300

    Add files via upload

---
+++ Day16/hibernateDemo/src/com/hibernatedemo/Main.java
@@ -0,0 +1,55 @@
+package com.hibernatedemo;
+
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(City.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+		
+		try{
+			session.beginTransaction();
+
+			 List<String> Cities =
+			 session.createQuery("select c.name from City c where c.population > 2000000").getResultList();
+
+			for(String city : Cities){
+				System.out.println(city);
+			}
+
+			//INSERT
+			/*
+			City city = new City();
+			city.setName("Düzce 10");
+			city.setCountryCode("TUR");
+			city.setDistrict("Karadeniz");
+			city.setPopulation(100000);
+			session.save(city);
+			*/
+
+			//UPDATE
+			/*
+			City city = session.get(City.class, 4086);
+			city.setPopulation(110000);
+			session.save(city);
+			*/
+			//CRUD Create Read Update Delete
+
+			//DELETE
+			/*
+			City city =session.get(City.class, 4086);
+			session.delete(city);
+			session.getTransaction().commit();
+			*/
+		}finally {
+			session.close();
+			factory.close();
+		}
+	}
+}
